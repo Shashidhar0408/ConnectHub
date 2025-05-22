88938dc58df04af31e0409f0b37336b07b818e31
Shashidhar0408 <shashidharchelimela12@gmail.com>
2025-05-22 16:01:00 +0530

    ready for deployment

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -17,6 +17,9 @@ export const connectToSocket = (server)  => {
 
     io.on("connection" , (socket)  => {
 
+        console.log("Something Connected");
+        
+
         socket.on("join-call", (path)  => {
             if(connections[path] === undefined){
                 connections[path] = []
@@ -24,7 +27,7 @@ export const connectToSocket = (server)  => {
             connections[path].push(socket.id)
 
             timeOnline[socket.id] = new Date();
-            for(let a = 0; a < connections[path].length; i++){
+            for(let a = 0; a < connections[path].length; a++){
                 io.to(connections[path][a]).emit("user-joined",socket.id, connections[path])
             }
             if (messages[path] !== undefined) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,35 @@
 import './App.css';
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
-import { ClassNames } from '@emotion/react';
 import { AuthProvider } from './contexts/AuthContext';
 import VideoMeetComponent from './pages/VideoMeet';
+import HomeComponent from './pages/home';
+import History from './pages/history';
 
 function App() {
   return (
-    <div className='App'>
-    <Router>
-<AuthProvider>
-    <Routes>
+    <div className="App">
 
-    <Route path = '/' element = {<LandingPage/>}></Route>
-    <Route path='/auth' element = {<Authentication/>}></Route>
-    <Route path='/:url'  element = {<VideoMeetComponent/>}></Route>
+      <Router>
+
+        <AuthProvider>
 
 
-    </Routes>
-    </AuthProvider>
-    </Router>
+          <Routes>
 
-</div>
+            <Route path='/' element={<LandingPage />} />
+
+            <Route path='/auth' element={<Authentication />} />
+
+            <Route path='/home's element={<HomeComponent />} />
+            <Route path='/history' element={<History />} />
+            <Route path='/:url' element={<VideoMeetComponent />} />
+          </Routes>
+        </AuthProvider>
+
+      </Router>
+    </div>
   );
 }
 
